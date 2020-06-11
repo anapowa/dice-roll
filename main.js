41557d6af5b34e93433cef90dcb686e9427613e0
Ana Ruiz Tovar <anapowa@gmail.com>
2020-06-11 12:40:28 -0400

    Completed program thousandRolls with output of index of count (adding up the number of times each roll result appeared in 1000 rolls).

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
-let dieRoll1 = Math.floor((Math.random() * 6) + 1)
-let dieRoll2 = Math.floor((Math.random() * 6) + 1)
 
-// let roll = function() {
-//     let randomNumElement = document.createElement("p")
-//     let textNodeP = document.createTextNode("")
-//     randomNumElement.appendChild(textNodeP)
-//     document.getElementById("dieRoll").appendChild(randomNumElement)
-// }
+let roll = function(n) {
+    let randomNumElement = document.createElement("p")
+    let textNodeP = document.createTextNode(n)
+    randomNumElement.appendChild(textNodeP)
+    document.getElementById("dieRoll").appendChild(randomNumElement)
+}
 
-// roll ()
+let thousandRolls = function() {
+    let count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    // roll (count)
+    let output=0
+    for (let i=0; i<1000; i++) {
+        if (i<1000) {
+            let dieRoll1 = Math.floor((Math.random() * 6) + 1)
+            let dieRoll2 = Math.floor((Math.random() * 6) + 1)
+            let diceRoll = dieRoll1 + dieRoll2 
+            // roll("die#1: " + dieRoll1);
+            // roll("die#2: " + dieRoll2);
+            // roll("Roll# " +i + ": " + diceRoll)
+        console.log("Roll# " +i + ": " + diceRoll)
+        output= output + diceRoll + ","
+        count[diceRoll] = count[diceRoll] + 1;
+        }
+        // tArray = output.split(",")
+        // console.log(output)
+    }
 
-console.log(dieRoll1)
-console.log(dieRoll2)
+    for (let i=0; i<=count.length-1; i++){
+        roll(i + ": " + count[i])
+    }
+}
 
-let diceRoll = dieRoll1 + dieRoll2 
-console.log(diceRoll)
+thousandRolls()
+
+
+
 
 
