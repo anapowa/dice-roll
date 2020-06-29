e65ac5ce683670311c16ece17399f33dda198294
Ana Ruiz Tovar <anapowa@gmail.com>
2020-06-29 12:26:24 -0400

    Corrected target for bar length using suggestion from Michelle H. (grader) in myKenzie comment.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
+let randomNumElement
 let roll = function(n) {
-    let randomNumElement = document.createElement("p")
+    randomNumElement = document.createElement("p")
     let textNodeP = document.createTextNode(n)
     randomNumElement.appendChild(textNodeP)
     document.getElementById("dieRoll").appendChild(randomNumElement)
@@ -8,7 +9,6 @@ let roll = function(n) {
 
 // let thousandRolls = function() {
     let count = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    // let output=0
     for (let i=0; i<1000; i++) {
         if (i<1000) {
             let dieRoll1 = Math.floor((Math.random() * 6) + 1)
@@ -18,12 +18,8 @@ let roll = function(n) {
             // roll("die#2: " + dieRoll2);
             // roll("Roll# " +i + ": " + diceRoll)
         console.log("Roll# " +i + ": " + diceRoll)
-        // output= output + diceRoll + ","
         count[diceRoll] = count[diceRoll] + 1;
         }
-        // count = count[i]
-        // tArray = output.split(",")
-        // console.log(output)
     }
     console.log(count)
 
@@ -31,12 +27,9 @@ let roll = function(n) {
         let barLength=count[i];
         console.log(barLength)
         roll(i + ": " + barLength);
-        document.getElementById("dieRoll").style.width= barLength + "px";
+        randomNumElement.style.width= barLength + "px";
     }
     
-// }
-
-// thousandRolls()
 
 
 
